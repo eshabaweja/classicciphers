b3acc4f6acb274beb7eaab848439500d65c9e46b
eshabaweja <bawejaesha02@gmail.com>
2023-04-17 15:06:01 +0530

    :worm: bhai kya kar raha hai tu

--- classicciphers/playfair.py
+++ classicciphers/playfair.py
@@ -1,85 +1,91 @@
-def encrypt(plaintext, key):
-    """
-    Encrypt plaintext using the Playfair cipher and the given key.
-
-    The plaintext is divided into pairs of letters and each pair is encrypted
-    using the 5x5 matrix generated from the key.
-
-    Args:
-        plaintext (str): The plaintext to encrypt.
-        key (str): The key to use for the 5x5 matrix.
-
-    Returns:
-        str: The encrypted ciphertext.
-
-    """
-    matrix = generate_matrix(key)
-    plaintext = plaintext.upper().replace('J', 'I').replace(' ', '')
-
-    # add an 'X' to the end if the length of plaintext is odd
-    if len(plaintext) % 2 == 1:
-        plaintext += 'X'
-
-    ciphertext = ''
-    for i in range(0, len(plaintext), 2):
-        a, b = plaintext[i:i+2]
-
-        # find the positions of the letters in the matrix
-        row1, col1 = find_letter(a, matrix)
-        row2, col2 = find_letter(b, matrix)
-
-        # encrypt the pair of letters
-        if row1 == row2:
-            # letters are in the same row, shift right
-            ciphertext += matrix[row1][(col1+1)%5] + matrix[row2][(col2+1)%5]
-        elif col1 == col2:
-            # letters are in the same column, shift down
-            ciphertext += matrix[(row1+1)%5][col1] + matrix[(row2+1)%5][col2]
+# Create a 5x5 matrix using a secret key
+def create_matrix(key):
+    key = key.upper()
+    matrix = [[0 for i in range(5)] for j in range(5)]
+    letters_added = []
+    row = 0
+    col = 0
+    # add the key to the matrix
+    for letter in key:
+        if letter not in letters_added:
+            matrix[row][col] = letter
+            letters_added.append(letter)
         else:
-            # letters form a rectangle, swap columns
-            ciphertext += matrix[row1][col2] + matrix[row2][col1]
-
-    return ciphertext
-
-
-def decrypt(ciphertext, key):
-    """
-    Decrypt ciphertext using the Playfair cipher and the given key.
-
-    The ciphertext is divided into pairs of letters and each pair is decrypted
-    using the 5x5 matrix generated from the key.
-
-    Args:
-        ciphertext (str): The ciphertext to decrypt.
-        key (str): The key to use for the 5x5 matrix.
-
-    Returns:
-        str: The decrypted plaintext.
-
-    """
-    matrix = generate_matrix(key)
-    plaintext = ''
-
-    for i in range(0, len(ciphertext), 2):
-        a, b = ciphertext[i:i+2]
-
-        # find the positions of the letters in the matrix
-        row1, col1 = find_letter(a, matrix)
-        row2, col2 = find_letter(b, matrix)
-
-        # decrypt the pair of letters
-        if row1 == row2:
-            # letters are in the same row, shift left
-            plaintext += matrix[row1][(col1-1)%5] + matrix[row2][(col2-1)%5]
-        elif col1 == col2:
-            # letters are in the same column, shift up
-            plaintext += matrix[(row1-1)%5][col1] + matrix[(row2-1)%5][col2]
+            continue
+        if col == 4:
+            col = 0
+            row += 1
         else:
-            # letters form a rectangle, swap columns
-            plaintext += matrix[row1][col2] + matrix[row2][col1]
+            col += 1
+    # Add the rest of the alphabet to the matrix
+    # A=65 ... Z=90
+    for letter in range(65, 91):
+        if letter == 74:  # I/J are in the same position
+            continue
+        if chr(letter) not in letters_added:  # Do not add repeated letters
+            letters_added.append(chr(letter))
 
-    # remove any trailing 'X' added during encryption
-    if plaintext[-1] == 'X':
-        plaintext = plaintext[:-1]
+    # print (len(letters_added), letters_added)
+    index = 0
+    for i in range(5):
+        for j in range(5):
+            matrix[i][j] = letters_added[index]
+            index += 1
+    return matrix
 
-    return plaintext
+    # Add fillers if the same letter is in a pair
+
+
+def separate_same_letters(message):
+    index = 0
+    while index < len(message):
+        l1 = message[index]
+        if index == len(message) - 1:
+            message = message + "X"
+            index += 2
+            continue
+        l2 = message[index + 1]
+        if l1 == l2:
+            message = message[: index + 1] + "X" + message[index + 1 :]
+        index += 2
+    return message
+
+
+# Return the index of a letter in the matrix
+# This will be used to know what rule (1-4) to apply
+def indexOf(letter, matrix):
+    for i in range(5):
+        try:
+            index = matrix[i].index(letter)
+            return (i, index)
+        except:
+            continue
+
+
+# Implementation of the playfair cipher
+# If encrypt=True the method will encrypt the message
+# otherwise the method will decrypt
+def encrypt(key, message, encrypt=True):
+    inc = 1
+    if encrypt == False:
+        inc = -1
+    matrix = create_matrix(key)
+    message = message.upper()
+    message = message.replace(" ", "")
+    message = separate_same_letters(message)
+    cipher_text = ""
+    for (l1, l2) in zip(message[0::2], message[1::2]):
+        row1, col1 = indexOf(l1, matrix)
+        row2, col2 = indexOf(l2, matrix)
+        if row1 == row2:  # Rule 2, the letters are in the same row
+            cipher_text += (
+                matrix[row1][(col1 + inc) % 5] + matrix[row2][(col2 + inc) % 5]
+            )
+        elif col1 == col2:  # Rule 3, the letters are in the same column
+            cipher_text += (
+                matrix[(row1 + inc) % 5][col1] + matrix[(row2 + inc) % 5][col2]
+            )
+        else:  # Rule 4, the letters are in a different row and column
+            cipher_text += matrix[row1][col2] + matrix[row2][col1]
+
+    return cipher_text
--- demo.py
+++ demo.py
@@ -1,11 +1,28 @@
 # a demo using caesar cipher
-from classicciphers import caesar
+from classicciphers import caesar, playfair
+import numpy as np
 
 plaintext = "HELLO WORLD"
 key = 5
 
 ciphertext = caesar.encrypt(plaintext, key)
-print(ciphertext)
+print("Plaintext: ",ciphertext)
 
 decrypted_plaintext = caesar.decrypt(ciphertext, key)
-print(decrypted_plaintext)
+print("Ciphertext: ",decrypted_plaintext)
+
+#################################    #################################
+
+# demo for playfair
+# Set up the key and plaintext
+key = "secret"
+plaintext = "MEETMEATTHEPARK"
+
+# Encrypt the plaintext
+ciphertext = playfair.encrypt(key, plaintext)
+print(f"Plaintext: {plaintext}")
+print(f"Ciphertext: {ciphertext}")
+
+# Decrypt the ciphertext
+decrypted_plaintext = playfair.encrypt(key,ciphertext,False)
+print(f"Decrypted plaintext:{decrypted_plaintext}")
